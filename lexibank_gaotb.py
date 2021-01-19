69789bdb05e9d1ed0a084662173c812e7c81bde3
Johann-Mattis List <LinguList@users.noreply.github.com>
2021-01-19 17:39:06 +0100

    Update lexibank_gaotb.py

--- lexibank_gaotb.py
+++ lexibank_gaotb.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank import Language, FormSpec
+from pylexibank import Language, FormSpec, Concept
 from pylexibank import progressbar
 
 from clldutils.misc import slug
@@ -12,6 +12,10 @@ class CustomLanguage(Language):
     Number = attr.ib(default=None)
     Chinese_Name = attr.ib(default=None)
 
+@attr.s
+class CustomConcept(Concept):
+    Number = attr.ib(default=None)
+    Chinese_Gloss = attr.ib(default=None)
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
@@ -19,7 +23,7 @@ class Dataset(BaseDataset):
     language_class = CustomLanguage
     form_spec = FormSpec(
         missing_data=("---",),
-        separators="/",
+        separators="/;",
         replacements=[(" ", "_")]
     )
 
@@ -28,27 +32,36 @@ class Dataset(BaseDataset):
         Convert the raw data to a CLDF dataset.
         """
         concepts = {}
-        for concept in self.concepts:
-            cid = '{0}_{1}'.format(concept['NUMBER'], slug(concept['ENGLISH']))
+        for concept in self.conceptlists[0].concepts.values():
+            cid = '{0}_{1}'.format(concept.number, slug(concept.english))
             args.writer.add_concept(
                 ID=cid,
-                Name=concept['ENGLISH'],
-                # TODO: Uncomment when ready
-                # Concepticon_ID=concept['CONCEPTICON_ID'],
-                # Concepticon_Gloss=concept['CONCEPTICON_GLOSS']
+                Name=concept.english,
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss,
+                Number=concept.number
             )
-            concepts[concept['ENGLISH']] = cid
+            concepts[concept.number] = cid
         languages = args.writer.add_languages(lookup_factory="Number")
         args.writer.add_sources()
+        
+        missingL, missingC = set(), set()
+        for row in self.raw_dir.read_csv('data.tsv', delimiter='\t', dicts=True):
+            lid = languages.get(row['LANGUAGE'])
+            cid = concepts.get(row['SID'])
+            if lid and cid and row["FORM"].strip()
+                args.writer.add_forms_from_value(
+                    Language_ID=lid,
+                    Parameter_ID=cid,
+                    Value=row["FORM"],
+                    Source='Sun1991'
+                )
+            if not lid:
+                missingL.add(lid)
+            if not cid:
+                missingC.add(cid)
+        for entry in missingL:
+            print('missing L {0}'.format(lid))
+        for entry in missingC:
+            print('missing C {0}'.format(cid))
 
-        for row in self.raw_dir.read_csv('data.tsv', delimiter='\t',
-                                         dicts=True):
-            for language, lid in languages.items():
-                form = row[language].strip()
-                if form and form != '---':
-                    args.writer.add_forms_from_value(
-                        Language_ID=lid,
-                        Parameter_ID=concepts[row['English']],
-                        Value=form,
-                        Source='Gao2020'
-                    )
